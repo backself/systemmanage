0c6606a156459398a13977fbeea98ad4db4827e1
wasd11203 <383684855@qq.com>
2023-04-11 18:02:19 +0800

    首页实时显示数据 -----

--- front-framework/src/views/main/dashboard/components/card/index.vue
+++ front-framework/src/views/main/dashboard/components/card/index.vue
@@ -1,46 +1,62 @@
 <template>
-  <div class="card-list">
-    <Row v-for="row in list" :key="row.id" :row="row" />
-  </div>
+	<div class="card-list">
+		<Row v-for="row in list" :key="row.id" :row="row" />
+	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent,ref } from 'vue'
-import {getOnlineData} from "@/api/dashboard/online"
-import Row from './row.vue'
+	import { defineComponent, ref, watch } from 'vue'
+	import Row from './row.vue'
+	import defaultData from './modules/defaultData'
 
-import param from './params'
-import defaultData from './modules/defaultData'
-
-export default defineComponent({
-  components: {
-    Row
-  },
-  setup() {
-	  
-    let list = ref([]);
-	
-	function initOnline(){
-		getOnlineData(param).then(function(res){
-			for (var i = 0; i < defaultData.length; i++) {
-				defaultData[i].data = res.data[i];
+	export default defineComponent({
+		components: {
+			Row
+		},
+		props: {// 接收由上级组件下发需要同步的属性
+			card_item: {
+				type: Object,
+				default: {
+					"id": Number, // 数据位置
+					"count": Number, // 数据内容
+					"type": Number // 修改方式
+				}
 			}
+
+		},
+		setup(props) {
+
+			let list = ref([]);
 			list.value = defaultData;
-		});
-	}
-	initOnline();
-    return {
-      list
-    }
-  }
-})
+
+			watch(props, (newProps) => {
+				console.log("新值：", newProps); //这里看到新值
+				let item = newProps.card_item;
+				let index = parseInt(item.id) - 1;
+				let dist = list.value[index];
+				console.log(dist.data);
+				if (item.type == 1) { // 等于1 做加法
+					console.log(dist);
+					dist.data = dist.data + item.count;
+				}
+				if (item.type == 0) { // 等于0 做减法
+					dist.data = (dist.data - item.count) > 0 ?
+						(dist.data - item.count) : 0;
+				}
+
+			});
+			return {
+				list
+			}
+		}
+	})
 </script>
 
 <style lang="scss" scoped>
-  .card-list {
-    width: calc(100% + 20px);
-    margin-left: -10px;
-    display: flex;
-    flex-wrap: wrap;
-  }
+	.card-list {
+		width: calc(100% + 20px);
+		margin-left: -10px;
+		display: flex;
+		flex-wrap: wrap;
+	}
 </style>
--- front-framework/src/views/main/dashboard/index.vue
+++ front-framework/src/views/main/dashboard/index.vue
@@ -1,27 +1,67 @@
 <template>
-  <div class="box">
-    <basic-template />
-    <Card />
-    <Charts />
-  </div>
+	<div class="box">
+		<basic-template />
+		<Card :card_item="cardItem" />
+		<Charts :chart_data="chartData" />
+	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import basicTemplate from './components/basic-template.vue'
-import Card from './components/card/index.vue'
-import Charts from './components/charts/index.vue'
-export default defineComponent({
-  components: {
-    Card,
-    Charts,
-    basicTemplate
-  }
-})
+	import { defineComponent, ref } from 'vue'
+	import basicTemplate from './components/basic-template.vue'
+	import Card from './components/card/index.vue'
+	import Charts from './components/charts/index.vue'
+	import { creatWS } from "@/api/dashboard/dashboard"
+
+	export default defineComponent({
+		components: {
+			Card,
+			Charts,
+			basicTemplate
+		},
+		setup() {
+			let cardItem = ref({});
+			let wsUrl = "ws://127.0.0.1:8003/messages/websocket/1";
+			let wsInstance = creatWS(wsUrl);
+			let chartData = {};
+
+			wsInstance.onopen = function (evt) {
+				console.log("建立连接");
+			}
+			wsInstance.onmessage = function (evt) {
+				
+				let msg = JSON.parse(evt.data);
+				let displayPosition = parseInt(msg.displayPosition);
+				if (displayPosition != 5) { // 只要不是报表信息就修改卡片信息
+					let item = {};
+					let content = JSON.parse(msg.content);
+					item.id = displayPosition;
+					item.count = content.count;
+					item.type = content.type;
+					cardItem.value = item;
+				};
+					
+				if (displayPosition == 5) { // 只要不是报表信息就修改卡片信息
+					let item = ref({});
+					chartData.value = item.content;
+				};
+			}
+
+
+			wsInstance.onclose = function (evt) {
+				console.log("断开连接");
+			}
+			
+			return {
+				cardItem,
+				chartData
+			};
+		}
+	})
 </script>
 
 <style lang="scss" scoped>
-  .box {
-    padding: 15px;
-  }
+	.box {
+		padding: 15px;
+	}
 </style>
